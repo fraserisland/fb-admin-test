73eaca1fa1105de3579c1dbc446898bb33f1504c
Fraser Solomon <fmsolomon1@gmail.com>
2019-12-08 18:52:50 +1000

    no json

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,9 +1,23 @@
+require("dotenv").config()
 const admin = require("firebase-admin")
 const serviceAccount = require("./serviceAccountKey.json") // Update this to your file
 
+const servicAcc = {
+  type: process.env.FB_TYPE,
+  project_id: process.env.FB_PROJECT_ID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID,
+  private_key: process.env.FB_PRIVATE_KEY,
+  client_email: process.env.FB_CLIENT_EMAIL,
+  client_id: process.env.FB_CLIENT_ID,
+  auth_uri: process.env.FB_AUTH_URI,
+  token_uri: process.env.FB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_CERT_UTL,
+  client_x509_cert_url: process.env.FB_CLIENT_PROVIDER_CERT_UTL
+}
+
 // Initialise the admin with the credentials
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(servicAcc),
   databaseURL: "https://dashboard-test-e60f0.firebaseio.com"
 })
 
