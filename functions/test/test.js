a62ae477cfbbce7a7f4d21851c3b2e76623f702a
Fraser Solomon <fmsolomon1@gmail.com>
2019-12-10 20:26:44 +1000

    remove comments

--- functions/test/test.js
+++ functions/test/test.js
@@ -13,23 +13,18 @@ const serviceAccount = {
   client_x509_cert_url: process.env.FB_CLIENT_PROVIDER_CERT_UTL
 }
 
-// Initialise the admin with the credentials
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://dashboard-test-e60f0.firebaseio.com"
 })
 
-// Set up an instance of the DB
 const db = admin.firestore()
 
-// exports.handler is required by netlify to process.
 exports.handler = (event, context, callback) => {
-  // wait for the record to be added
   db.collection("orders").add({
     name: "fake order"
   })
 
-  // Return a callback witha 200 response and a message.
   return callback(null, {
     statusCode: 200,
     body: JSON.stringify({
